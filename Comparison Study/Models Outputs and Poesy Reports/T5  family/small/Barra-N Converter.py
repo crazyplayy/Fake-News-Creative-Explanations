ae6ab756d31153ec227ee81c45c997ab63cad29a
crazyplayy <48414755+crazyplayy@users.noreply.github.com>
2022-10-04 18:52:15 +0100

    Add files via upload

---
+++ Comparison Study/Models Outputs and Poesy Reports/T5  family/small/Barra-N Converter.py
@@ -0,0 +1,23 @@
+
+i = 1
+
+while i < 11:
+    abrir = "output" + str(i) + ".txt"
+    guardar = str(i) + "normal.txt"
+
+    file = open(abrir, "r", encoding= 'utf-8')
+    string_list = file.readlines()
+
+    texto= ''
+    file = open(guardar, "w", encoding= 'utf-8')
+    for line in string_list:
+        line = line.replace('  ','\n')
+        #line = line.replace('\n','  ')
+        texto += line
+        
+    file.write(texto)
+    file.close()
+
+    i += 1
+
+print("donezo")
